7a9844e2522fffd5a2cf8f84007f682bd5e8d7da
alimdfaiz <144793353+alimdfaiz@users.noreply.github.com>
2023-12-01 11:40:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ let value1 = parseFloat(prompt("Enter the value1"));
 let value2 = value1*2;
 let value3 = parseFloat(prompt("Enter the value3"));
 let result = value2 + value3;
-alert("The final result is: " + result.);
+alert("The final result is: " + result);
